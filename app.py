5a0dabb7916350c922fae128d2e7eee197fa8d47
Vedant3993 <vedantfulsoundar9@gmail.com>
2025-05-31 13:01:25 +0530

    done

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import joblib 
+import numpy as np
+
+model=joblib.load("reg_model.joblib")
+
+st.title("Job Package Prediction Based  on CGPA")
+st.write("Enter your CGPA to predict the expected job package:")
+
+cgpa=st.number_input("CGPA", min_value=0.0, max_value=10.0,step=0.1)
+
+if st.button("Predict Package"):
+    input_data = np.array([[cgpa]])
+
+    prediction =model.predict(input_data)
+    predicted_value= float(prediction[0])
+
+    st.success(f"Predicted Package:₹{predicted_value:,.2f}LPA")
